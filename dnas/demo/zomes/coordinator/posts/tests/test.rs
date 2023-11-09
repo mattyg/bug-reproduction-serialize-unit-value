978f97038d122a59fcbedb9216e2cc2f616f70a5
Matt Gabrenya <matt@buildyourweb.app>
2023-11-09 08:50:47 -0800

    chore: cleanup

--- dnas/demo/zomes/coordinator/posts/tests/test.rs
+++ dnas/demo/zomes/coordinator/posts/tests/test.rs
@@ -1,7 +1,8 @@
-use hdk::prelude::*;
-use holochain::{conductor::{config::ConductorConfig, api::error::ConductorApiError}, sweettest::*, prelude::DnaFile};
-use holochain::test_utils::consistency_60s;
-  
+use holochain::{
+    conductor::config::ConductorConfig,
+    sweettest::{SweetConductor, SweetDnaFile},
+};
+
 #[tokio::test(flavor = "multi_thread")]
 async fn can_serialize_unit_value() {
     // Use prebuilt dna file
@@ -11,21 +12,18 @@ async fn can_serialize_unit_value() {
     let dna = SweetDnaFile::from_bundle(&dna_path).await.unwrap();
 
     // Set up conductors
-    let mut conductor: SweetConductor= SweetConductor::from_config(ConductorConfig::default()).await;
+    let mut conductor: SweetConductor =
+        SweetConductor::from_config(ConductorConfig::default()).await;
     let app = conductor.setup_app("demo", &[dna]).await.unwrap();
     let (alice,) = app.into_tuple();
 
     // Alice gets a u32 response
-    let value: u32 = conductor
-        .call(&alice.zome("posts"), "get_u32", ())
-        .await;
-    
+    let value: u32 = conductor.call(&alice.zome("posts"), "get_u32", ()).await;
+
     assert_eq!(value, 1);
 
     // Alice gets a usize response
-    let value: usize = conductor
-        .call(&alice.zome("posts"), "get_usize", ())
-        .await;
-    
+    let value: usize = conductor.call(&alice.zome("posts"), "get_usize", ()).await;
+
     assert_eq!(value, 1);
 }
